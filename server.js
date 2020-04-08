015479db5c0790217f3b1afc7f5fd5e2137365b1
Guillaume Groult <guillaume.groult3@orange.fr>
2020-04-08 09:54:53 +0200

    Fix CORS permission

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const path = require('path')
+
+app.use(cors());
 
 app.use('/',express.static(path.join(__dirname,'/assets')))
 
